41b97466cadccd6473a65c5d6358a3e43c74ac98
Sam Olukotun <otsam88@gmail.com>
2017-04-10 19:06:35 -0700

    Gets and renders messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,6 +7,7 @@
   // bold all messages from those users
 
 window.url = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+var $chatContainer;
 
 var messages = [];
 
@@ -15,10 +16,12 @@ var messages = [];
 
 // attach on ready document handler into document.ready listener
 $(document).ready(function() {
-
   var $main = $('#main');
+  $chatContainer = $('#chats');
+
   // attach event handler on send message button
   $('#send-message').click(sendMessage);
+
   // attach event handler on refresh button
   $('#refresh').click(getMessages);
 
@@ -38,10 +41,29 @@ var sendMessage = function() {
 var getMessages = function() {
   $.getJSON(window.url, function(data) {
     messages = data.results;
-  });
+  }).done(renderMessages);
 };
 
-var renderMessages = function() {
+// <div class="message">
+//   <a class="user" data-user=author-name>user-name-here</a>
+//   <time class="timestamp"></time>
+//   <div class='message-body'>message-body-here</div>
+// </div>
 
+var renderMessages = function() {
+  _.forEach(messages, function(messageObj) {
+    console.log("inside forEach");
+    var $messageDiv = $('<div class="message"></div>');
+    var $userName = $('<a class="user"></a>');
+    var $messageBody = $('<div class="message-body"></div>');
+    var $time = $('<time class="timestamp"></time>');
+
+    $userName.text(messageObj.username);
+    $messageBody.text(messageObj.text);
+    $time.text(messageObj.createdAt);
+
+    $messageDiv.append($userName, $messageBody, $time);
+    $chatContainer.append($messageDiv);
+  });
 };
 
