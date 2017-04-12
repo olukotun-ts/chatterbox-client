dc55e647bb66cffe0eede1fd244c2bfeefc82ec8
Sam Olukotun <otsam88@gmail.com>
2017-04-11 17:21:00 -0700

    Refactor. Meets all spec.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,140 +5,140 @@
 // x allow for chat rooms
 // x 'befriend users' functionality by clicking on usernames
   // x bold all messages from those users
+// x wrap our methods according to the spec runner expectations
 
-window.url = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
-var $chatContainer;
-var rooms = {};
-var $roomName;
-var messages = [];
-var username;
-var friends = [];
+// Extra Objectives
+// add new room name to list directly instead of reading it on refresh
+// the example and the css we're given mention a spinner loading gif?
+// Make everything more dynamic
+// General
 
-// onLoad event handler function
-  // initial call to refresh handler (get messages)
+var app = {
 
-// attach on ready document handler into document.ready listener
-$(document).ready(function() {
-  var $main = $('#main');
-  $chatContainer = $('#chats');
+  init: function() {
+    app.server = 'http://parse.hrr.hackreactor.com/chatterbox/classes/messages';
+    app.rooms = {};
+    app.$roomName;
+    app.messages = [];
+    app.username;
+    app.friends = [];
 
-  // attach event handler on send message button
-  $('#send-message').click(sendMessage);
+    // jQuery selectors
+    app.$chatContainer = $('#chats');
 
-  // attach event handler on refresh button
-  $('#refresh').click(function() {getMessages();});
+    $('form').submit(app.handleSubmit);
 
-  // attach event handler on room drop-down list
-  $('select').change(function() {
-    $roomName = $(this).find(':selected').text();
-    if ($roomName === 'Add New Room...') {
-      $roomName = prompt('New room name');
-    } else {
-      getMessages($roomName);
-    }
-  });
+    $('#refresh').click(function() {app.fetch();});
 
-  $chatContainer.click('.username', function(event) {
+    $('select').change(function() {
+      app.$roomName = $(this).find(':selected').text();
+      if (app.$roomName === 'Add New Room...') {
+        app.$roomName = prompt('New room name');
+      } else {
+        app.fetch(app.$roomName);
+      }
+    });
+
+    app.$chatContainer.click('.username', app.handleUsernameClick);
+
+  },
+
+  handleUsernameClick: function(event) {
     var userClicked = event.target.text;
     if (userClicked !== undefined) {
-      if (friends.indexOf(userClicked) === -1) {
-        friends.push(userClicked);
+      if (app.friends.indexOf(userClicked) === -1) {
+        app.friends.push(userClicked);
       }
     }
-  });
+  },
 
-});
+  handleSubmit: function(event) {
+    event.preventDefault();
+    if (!app.username) {
+      app.username = window.location.search.split('=')[1];
+    }
+    var data = {};
+    data.text = $('#user-message').val();
+    data.username = app.username;
+    data.roomname = app.$roomName;
+    app.send(data);
+  },
 
-// var exampleMessage = {
-//   username: 'shawndrost',
-//   text: 'trololo',
-//   roomname: '4chan'
-// };
+  send: function(messageObj) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(messageObj),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('Message successfully sent.');
+      },
+      error: function(data) {
+        console.error('Message failed to post with data: ' + data);
+      }
+    });
+  },
 
-var sendMessage = function() {
-  if (!username) {
-    username = window.location.search.split('=')[1];
+  fetch: function(roomname) {
+    var query = {order: '-createdAt'};  // Return msgs in desc order of time.
+    if (roomname !== 'Select a Room') {  // If user selects a room, only get msgs from that room.
+      query.where = {roomname: roomname};
+    }
+
+    $.ajax({
+      url: app.server,
+      data: query,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        app.messages = data.results;
+        app.clearMessages();
+        app.renderMessages();
+      },
+      error: function(data) {
+        console.error('getMessage failure with data ' + data);
+      }
+    });
+  },
+
+  renderMessage: function(messageObj) {
+      var $messageDiv = $('<div class="chat"></div>');
+      var $userName = $('<a class="username"></a>');
+      var $messageBody = $('<div class="message-body"></div>');
+      var $time = $('<time class="timestamp"></time>');
+
+      $userName.text(messageObj.username);
+      $messageBody.text(messageObj.text);
+      $time.text(messageObj.createdAt);
+
+      if(app.friends.indexOf(messageObj.username) !== -1) {
+        $messageDiv.addClass('friend');
+      }
+
+      $messageDiv.append($userName, $messageBody, $time);
+      app.$chatContainer.append($messageDiv);
+
+      // populate rooms object
+      if(!app.rooms[messageObj.roomname] && messageObj.roomname !== undefined) {
+        app.rooms[messageObj.roomname] = messageObj.roomname;
+        var $room = $('<option></option>');
+        $room.text(messageObj.roomname);
+        $('#room-selector select').append($room);
+      }
+  },
+
+  renderMessages: function() {
+    app.messages.forEach(app.renderMessage);
+  },
+
+  renderRoom: function(name) {
+    var $option = $('<option></option>');
+    $option.text(name);
+    $('#roomSelect').append($option);
+  },
+
+  clearMessages: function() {
+    app.$chatContainer.empty();
   }
-  var data = {};
-  data.text = $('#user-message').val();
-  data.username = username;
-  data.roomname = $roomName;
 
-  $.ajax({
-    url: window.url,
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json',
-    success: function(data) {
-      console.log('Message successfully sent.');
-    },
-    error: function(data) {
-      console.error('Message failed to post with data: ' + data);
-    }
-  });
 };
-
-var getMessages = function(roomname) {
-/*
-  $.getJSON(window.url, function(data) {
-    messages = data.results;
-  }).done(renderMessages);
-*/
-  // @tapOut
-  var query = {order: '-createdAt'};  // Return msgs in desc order of time.
-
-  if (roomname !== 'Select a Room') {  // If user selects a room, only get msgs from that room.
-    query.where = {roomname: roomname};
-  }
-
-  $.ajax({
-    url: url,
-    data: query,
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      messages = data.results;
-      $chatContainer.empty();
-      renderMessages();
-    },
-    error: function(data) {
-      console.error('getMessage failure with data ' + data);
-    }
-  });
-};
-
-// <div class="message">
-//   <a class="user" data-user=author-name>user-name-here</a>
-//   <time class="timestamp"></time>
-//   <div class='message-body'>message-body-here</div>
-// </div>
-
-var renderMessages = function() {
-  _.forEach(messages, function(messageObj) {
-    // console.log("inside forEach");
-    var $messageDiv = $('<div class="chat"></div>');
-    var $userName = $('<a class="username"></a>');
-    var $messageBody = $('<div class="message-body"></div>');
-    var $time = $('<time class="timestamp"></time>');
-
-    $userName.text(messageObj.username);
-    $messageBody.text(messageObj.text);
-    $time.text(messageObj.createdAt);
-
-    if(friends.indexOf(messageObj.username) !== -1) {
-      $messageDiv.addClass('friend');
-    }
-
-    $messageDiv.append($userName, $messageBody, $time);
-    $chatContainer.append($messageDiv);
-
-    // populate rooms object
-    if(!rooms[messageObj.roomname] && messageObj.roomname !== undefined) {
-      rooms[messageObj.roomname] = messageObj.roomname;
-      var $room = $('<option></option>');
-      $room.text(messageObj.roomname);
-      $('#room-selector select').append($room);
-    }
-  });
-};
-
